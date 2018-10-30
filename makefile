9e3fa5ab62a5a319819fb17612fb4bbdeffbc405
KReload <billy-liang.wei@etu.upmc.fr>
2018-10-30 18:17:13 +0100

    correct

--- makefile
+++ makefile
@@ -2,7 +2,7 @@
 
 #Makefile
 all: nrutil.o
-	#gcc sigdelta.c nrutil.o -o sigdelta
+	gcc sigdelta.c nrutil.o -o sigdelta -lm
 	gcc testOpen.c nrutil.o -o testOpen
 
 nrutil.o:
--- sigdelta.c
+++ sigdelta.c
@@ -1,18 +1,20 @@
+#include "nrdef.h"
 #include "sigdelta.h"
 #include "nrutil.h"
-#include "nrdef.h"
+#include <math.h>
+
 
 void sigmaDelta(char* filename1, char* filename2)
 {
   long nrl,nrh,ncl,nch;
-  uint8** I0 = LoadPGM_ui8matrix(char *filename,&nrl,&nrh,&ncl,&nch);
-  uint8** I1 = LoadPGM_ui8matrix(char *filename,&nrl,&nrh,&ncl,&nch);
+  uint8** I0 = LoadPGM_ui8matrix(filename1,&nrl,&nrh,&ncl,&nch);
+  uint8** I1 = LoadPGM_ui8matrix(filename2,&nrl,&nrh,&ncl,&nch);
   uint8** M0 = I0;
-  uint8** M1;
-  uint8** V0 = Vmin;
-  uint8** V1;
-  uint8** O1;
-  uint8** E1;
+  uint8** M1 = ui8matrix(nrl,nrh,ncl,nch);
+  uint8** V0 = ui8matrix(nrl,nrh,ncl,nch);
+  uint8** V1 = ui8matrix(nrl,nrh,ncl,nch);
+  uint8** O1 = ui8matrix(nrl,nrh,ncl,nch);
+  uint8** E1 = ui8matrix(nrl,nrh,ncl,nch);
 
   //Step 1 : M1 estimation
   for(int i = 0; i < nrh; i++)
@@ -32,7 +34,7 @@ void sigmaDelta(char* filename1, char* filename2)
   for(int i = 0; i < nrh; i++)
     {
       for(int j = 0; j < nch; j++)
-	O1[i][j] = abs(M1[i][j] - I1[i][j]); 
+	O1[i][j] = abs(M1[i][j] - I1[i][j]);
     }
 
   //Step 3 : V1 Update and Clamping
@@ -49,7 +51,7 @@ void sigmaDelta(char* filename1, char* filename2)
 	  V1[i][j] = max(min(V1[i][j],VMAX),VMIN); //Clamp
 	}
     }
-  
+
   //Step 4 : E1 Estimation
   for(int i = 0; i < nrh; i++)
     {
@@ -60,9 +62,9 @@ void sigmaDelta(char* filename1, char* filename2)
 	  else
 	    E1[i][j] = 1;
 	}
-      
+
     }
-  
+
 }
 
 
