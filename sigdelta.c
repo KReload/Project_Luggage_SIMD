1545402bcd506e532353e7c05d214f5adcb7744b
KReload <billy-liang.wei@etu.upmc.fr>
2018-10-30 17:40:04 +0100

    constante makefile include

--- sigdelta.c
+++ sigdelta.c
@@ -1,3 +1,6 @@
+#include "sigmaDelta.h"
+#include "nrutil.h"
+
 void sigmaDelta(char* filename1, char* filename2)
 {
   long nrl,nrh,ncl,nch;
@@ -5,9 +8,8 @@ void sigmaDelta(char* filename1, char* filename2)
   uint8** I1 = LoadPGM_ui8matrix(char *filename,&nrl,&nrh,&ncl,&nch);
   uint8** M = I0;
 
-  
-}
 
+}
 
 int main(void)
 {
