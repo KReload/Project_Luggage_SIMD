ac7cca36d3007174b609fce7778f7fbb04548ee8
KReload <billy-liang.wei@etu.upmc.fr>
2018-10-30 18:27:55 +0100

    inversion image

--- testOpen.c
+++ testOpen.c
@@ -1,11 +1,19 @@
 #include "include/nrdef.h"
 #include "include/nrutil.h"
+#include <stdio.h>
 
 
 int main(void) {
-
+  //Prend une image et inverse les niveaux de gris
   long nrl, nrh, ncl,nch;
   uint8**img = LoadPGM_ui8matrix("./hall/hall000000.pgm",&nrl,&nrh,&ncl,&nch);
+  for(int i = 0; i<nrh;i++) {
+    for(int j = 0; j<nch;j++) {
+      img[i][j] = 255 - img[i][j];
+    }
+  }
+
+  SavePGM_ui8matrix(img, nrl, nrh, ncl, nch, "hall0Inv.pgm");
 
   return 0;
 }
