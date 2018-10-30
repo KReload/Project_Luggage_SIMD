97747c7b8f12eee9a1284a6cdc7e5f477b1727bb
BESSIERES Fabien <3407573@pc4016.polytech.upmc.fr>
2018-10-30 18:02:14 +0100

    sigma_delta do alloc

--- sigdelta.c
+++ sigdelta.c
@@ -1,3 +1,7 @@
+#include "sigdelta.h"
+#include "nrutil.h"
+#include "nrdef.h"
+
 void sigmaDelta(char* filename1, char* filename2)
 {
   long nrl,nrh,ncl,nch;
@@ -5,8 +9,10 @@ void sigmaDelta(char* filename1, char* filename2)
   uint8** I1 = LoadPGM_ui8matrix(char *filename,&nrl,&nrh,&ncl,&nch);
   uint8** M0 = I0;
   uint8** M1;
-  uint8** V = Vmin;
+  uint8** V0 = Vmin;
+  uint8** V1;
   uint8** O1;
+  uint8** E1;
 
   //Step 1 : M1 estimation
   for(int i = 0; i < nrh; i++)
@@ -26,7 +32,7 @@ void sigmaDelta(char* filename1, char* filename2)
   for(int i = 0; i < nrh; i++)
     {
       for(int j = 0; j < nch; j++)
-	O1[i][j] = M1[i][j] - I1[i][j]; 
+	O1[i][j] = abs(M1[i][j] - I1[i][j]); 
     }
 
   //Step 3 : V1 Update and Clamping
@@ -34,13 +40,27 @@ void sigmaDelta(char* filename1, char* filename2)
     {
       for(int j = 0; j < nch; j++)
 	{
-	  if(M0[i][j] < I1[i][j])
-	    M1[i][j] = M0[i][j] + 1;
-	  else if(M0[i][j] > I1[i][j])
-	    M1[i][j] = M0[i][j] - 1;
+	  if(V0[i][j] < N*O1[i][j])
+	    V1[i][j] = V0[i][j] + 1;
+	  else if(V0[i][j] > N*O1[i][j])
+	    M1[i][j] = V0[i][j] - 1;
 	  else
-	  ;
+	    V1[i][j] = V0[i][j];
+	  V1[i][j] = max(min(V1[i][j],VMAX),VMIN); //Clamp
 	}
+    }
+  
+  //Step 4 : E1 Estimation
+  for(int i = 0; i < nrh; i++)
+    {
+      for(int j = 0; j < nch; j++)
+	{
+	  if(O1[i][j] < V1[i][j])
+	    E1[i][j] = 0;
+	  else
+	    E1[i][j] = 1;
+	}
+      
     }
   
 }
