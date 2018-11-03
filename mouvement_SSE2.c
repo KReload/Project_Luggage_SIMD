8af44fa202963d2f52756056bfd04fa4e2cd38ba
Billy WEI <kurospade@gmail.com>
2018-11-03 04:21:12 +0100

    mouvementSSE2

--- mouvement_SSE2.c
+++ mouvement_SSE2.c
@@ -14,55 +14,45 @@
 
 
 
-/*
-vuint8** load_uint8_to_vui8matrix(uint8**a, long nrl, long nrh, long ncl, long nch) {
 
-  //Convertion de uint8 matrix à vuint8 matrix (qui est une _m128i matrice)
-  vuint8** res = vui8matrix(nrl/16, nrh/16 +1, ncl/16, nch/16 + 1);
-  uint8* temp;
-  
-  for(int i=0;i<(nrh-nrl)/16 + 1;i++){
-    for(int j=0;j<(nch-ncl)/16 + 1;j++) {
-      res[i] = _mm_load_si128(a[i][j*16]);
-    }
-  }
-
-  return res;
-}
-*/
-/*
-void frameDifference(char* filename0, char* filename1)
+vuint8** frameDifference(char* filename0, char* filename1, long* nrl,long* nrh,long* ncl, long* nch)
 {
-  long nrl, nrh, ncl, nch;
-  uint8** I0 = LoadPGM_ui8matrix(filename0, &nrl, &nrh, &ncl, &nch);
-  uint8** I1 = LoadPGM_ui8matrix(filename1, &nrl, &nrh, &ncl, &nch);
-  vuint8** O = vui8matrix(nrl, nrh, ncl, nch);
-  vuint8** E = vui8matrix(nrl, nrh, ncl, nch);
+  vuint8** I0 = LoadPGM_vui8matrix(filename0, nrl, nrh, ncl, nch);
+  vuint8** I1 = LoadPGM_vui8matrix(filename1, nrl, nrh, ncl, nch);
+  vuint8** O = vui8matrix(*nrl, *nrh, *ncl, *nch);
+  vuint8** E = vui8matrix(*nrl, *nrh, *ncl, *nch);
 
   //vecteur 16x8 bits chaque octet contient THETA
   vuint8 theta = init_vuint8((uint8)THETA);
   
   
-  for(int i = 0; i < nrh; i++)
+  for(int i = 0; i < *nrh; i++)
     {
-      for(int j = 0; j < nch; j++)
+      for(int j = 0; j < *nch; j++)
       {
-        O[i][j] = abs(_mm_sub_epi8(I1[i][j],I0[i][j]));
+        O[i][j] = _mm_sub_epi8((__m128i)I1[i][j],(__m128i)I0[i][j]);
 
-        if(_mm_cmplt_epi8(O[i][j], theta))
-          _mm_store_si128(E[i][j], 0);
+        if(0)
+          _mm_store_si128((__m128i*)&E[i][j], _mm_set_epi8(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0));
         else
-          _mm_store_si128(E[i][j], 1);
+          _mm_store_si128((__m128i*)&E[i][j], _mm_set_epi8(255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255));
       }
     }
+  
+  return E;
 }
-*/
+
 
 
 int main(void)
 {
   long nrl, nrh, ncl,nch;
-  vuint8**img = LoadPGM_vui8matrix("./hall/hall000000.pgm",&nrl,&nrh,&ncl,&nch);
-  SavePGM_vui8matrix(img, nrl, nrh, ncl, nch, "hall0SIMDtest.pgm");
+  //vuint8**img = LoadPGM_vui8matrix("./hall/hall000000.pgm",&nrl,&nrh,&ncl,&nch);
+  //vuint8**img2 = LoadPGM_vui8matrix("./hall/hall000000.pgm",&nrl,&nrh,&ncl,&nch);
+
+  vuint8**E = frameDifference("./hall/hall000000.pgm", "./hall/hall000001.pgm", &nrl,&nrh,&ncl,&nch);
+
+  SavePGM_vui8matrix(E, nrl, nrh, ncl, nch, "hall1SIMDtest.pgm");
+
 }
 
